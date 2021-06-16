600a85a481da8e3826b50a166b6ffcb392e7319d
Guillaume Martigny <guillaume.martigny@gmail.com>
2021-06-16 20:45:34 +0200

    Add photo to tiles, split CSS code, auto nav

--- src/main.js
+++ src/main.js
@@ -6,6 +6,31 @@ window.addEventListener("load", () => {
         document.body.classList.add("ready");
     });
 
+    const nav = document.getElementById("nav");
+    const sections = document.body.querySelectorAll("h2");
+
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "#";
+    link.innerText = "Haut";
+    li.appendChild(link);
+    nav.appendChild(li);
+
+    sections.forEach((title) => {
+        const anchor = document.createElement("span");
+        anchor.className = "anchor";
+        const id = title.innerText.toLowerCase().replace(/ /g, "_").normalize("NFD").replace(/\W/g, "");
+        anchor.id = id;
+        title.appendChild(anchor);
+
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = `#${id}`;
+        link.innerText = title.innerText;
+        li.appendChild(link);
+        nav.appendChild(li);
+    });
+
     const viewHeight = window.innerHeight;
     let scroll = window.scrollY;
     document.body.classList.toggle("scrolled", scroll > 50);
